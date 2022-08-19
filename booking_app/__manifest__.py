170662e8c54a4f3e7ba25d589397c0c8249641e5
trau <oscarnguyen126@gmail.com>
2022-08-19 17:23:18 +0700

    Email config

--- booking_app/__manifest__.py
+++ booking_app/__manifest__.py
@@ -2,11 +2,11 @@
     'name': 'Booking room',
     'version': '1.0',
     'application': True,
-    'depends': ['base', 'mail'],
+    'depends': ['base', 'mail','sale'],
     'data': ['security/ir.model.access.csv',
              'views/room_views.xml',
              'views/room_booking_menu.xml',
              'views/room_booking_views.xml',
-             'data/mail_template.xml'],
+             'data/email_template.xml'],
     'license': 'GPL-3',
 }
